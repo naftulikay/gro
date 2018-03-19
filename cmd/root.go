885c88c137c6ff57198cbc5c6ec1ef966278738c
Naftuli Kay <me@naftuli.wtf>
2018-03-18 18:26:32 -0700

    find!

--- cmd/root.go
+++ cmd/root.go
@@ -3,7 +3,6 @@ package cmd
 import (
   "fmt"
   "os"
-  "path/filepath"
 
   "github.com/spf13/cobra"
 )
@@ -12,49 +11,12 @@ var rootCmd = &cobra.Command{
   Use:   "gro",
   Short: "A crommand line utilidang in grolang.",
   Long: "A crommand line utilidang in grolang.",
-  Run: func(cmd *cobra.Command, args []string) {
-    f, err := os.Open(".ansible")
-
-    if err != nil {
-      fmt.Println("error")
-      os.Exit(1)
-    }
-
-    fmt.Println("starting directory:", f.Name())
-
-    rlistdir(f)
-  },
+  Run: func(cmd *cobra.Command, args []string) {},
 }
 
 func Execute() {
   if err := rootCmd.Execute(); err != nil {
     fmt.Println(err)
     os.Exit(1)
-  }
-}
-
-func rlistdir(d *os.File) {
-  fmt.Println(d.Name() + "/")
-
-  fileinfos, err := d.Readdir(0)
-
-  if err != nil {
-    fmt.Println("unable to list directory ", d.Name())
-    os.Exit(1)
-  }
-
-  for _, info := range fileinfos {
-    path := filepath.Join(d.Name(), info.Name())
-
-    if info.IsDir() {
-      f, err := os.Open(path)
-      if err != nil {
-        fmt.Println("unable to open", path)
-        os.Exit(1)
-      }
-      rlistdir(f)
-    } else {
-      fmt.Println(path)
-    }
   }
 }
